f83d8e8377af65ba208a672d6a759b4fe45e7cfc
RodrigoAQuiroga <122882663+RodrigoAQuiroga@users.noreply.github.com>
2023-04-12 12:09:55 -0300

    Arreglos de funciones y del estilo

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function indemnizacionGeneral(){
         let rubroB = document.createElement("p");
         rubroB.setAttribute("id", "indeGeneral")
 
-        rubroB.innerText = "Según su salario de $"+ salario + ", y por sus "+ (yearFinal - yearInicial) +" años de relación, le corresponde una indemnización aproximada de $" + ((yearFinal - yearInicial)-1) * salario + " de acuerdo al art. 245 LCT";
+        rubroB.innerText = "Según su salario de $"+ salario + ", y por sus "+ ((yearFinal - yearInicial)-1) +" años de relación, le corresponde una indemnización aproximada de $" + ((yearFinal - yearInicial)-1) * salario + " de acuerdo al art. 245 LCT";
 
         document.getElementById("showRubro").appendChild(rubroB); 
     }
@@ -73,21 +73,55 @@ function indemnizacionGeneral(){
     
     //Inicio Indemnizacion Integración del Mes
 
-    let claseMeses = [31, 30, 28]
+    let claseMeses = [31, 30, 28, 29]
 
     if (diaFinal>=1 && diaFinal<31 && mesFinal == 1 || mesFinal == 3 || mesFinal == 5 || mesFinal == 7 || mesFinal == 8 || mesFinal == 10 || mesFinal == 12) {
 
         let rubroC = document.createElement("p");
         rubroC.setAttribute("id", "indeIntegra")
 
-        rubroC.innerText = "Por intergración del mes de despido, debido a que le faltaban " + (claseMeses[0]- diaFinal) + " días y su sueldo proporcional era de $"+ (salario/claseMeses[0]) +" para que concluya el mes, le corresponde aproximadamente de acuerdo al art. 233 LCT $" + (salario/claseMeses[0])*(claseMeses[0]-diaFinal);
+        rubroC.innerText = "Por intergración del mes de despido, debido a que le faltaban " + (claseMeses[0]- diaFinal) + " días para que concluya el mes, y su sueldo proporcional era de $"+ (salario/claseMeses[0]) +", le corresponde aproximadamente de acuerdo al art. 233 LCT $" + (salario/claseMeses[0])*(claseMeses[0]-diaFinal);
 
         document.getElementById("showRubro").appendChild(rubroC); 
+        
+    } else if (diaFinal>=1 && diaFinal<31 && mesFinal == 4 || mesFinal == 6 || mesFinal == 9 || mesFinal == 11) {
 
+        let rubroC = document.createElement("p");
+        rubroC.setAttribute("id", "indeIntegra")
 
+        rubroC.innerText = "Por intergración del mes de despido, debido a que le faltaban " + (claseMeses[1]- diaFinal) + " días para que concluya el mes, y su sueldo proporcional era de $"+ (salario/claseMeses[1]) +", le corresponde aproximadamente de acuerdo al art. 233 LCT $" + (salario/claseMeses[1])*(claseMeses[1]-diaFinal);
+
+        document.getElementById("showRubro").appendChild(rubroC); 
+        
+    } else if (diaFinal>=1 && diaFinal<31 && mesFinal == 2 && (yearFinal%4) === 0) {
+
+        let rubroC = document.createElement("p");
+        rubroC.setAttribute("id", "indeIntegra")
+
+        rubroC.innerText = "Por intergración del mes de despido, debido a que le faltaban " + (29- diaFinal) + " días para que concluya el mes, y su sueldo proporcional era de $"+ (salario/29) +", le corresponde aproximadamente de acuerdo al art. 233 LCT $" + (salario/29)*(29-diaFinal);
+
+        document.getElementById("showRubro").appendChild(rubroC); 
+        
+    } else if (diaFinal>=1 && diaFinal<31 && mesFinal == 2) {
+
+        let rubroC = document.createElement("p");
+        rubroC.setAttribute("id", "indeIntegra")
+
+        rubroC.innerText = "Por intergración del mes de despido, debido a que le faltaban " + (claseMeses[2]- diaFinal) + " días para que concluya el mes, y su sueldo proporcional era de $"+ (salario/claseMeses[2]) +", le corresponde aproximadamente de acuerdo al art. 233 LCT $" + (salario/claseMeses[2])*(claseMeses[2]-diaFinal);
+
+        document.getElementById("showRubro").appendChild(rubroC);
         
     }
 }
+
+function erase() {
+    
+        let a = document.getElementById("indeGeneral");
+        a.remove();
+        let b = document.getElementById("indeIntegra");
+        b.remove();
+    }
+
 
 
 
